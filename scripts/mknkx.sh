b67e251266078697e68bfbcd1adda2af453ad941
0xFireball <0xFireball@outlook.com>
2017-02-06 22:49:18 -0800

    include babel pf

--- scripts/mknkx.sh
+++ scripts/mknkx.sh
@@ -6,7 +6,7 @@ mkdir out || cd lib
 # browserify
 
 browserify ./jsdom_nkx.js -o ../out/jsdom_nkx_pack.js
-browserify ./ui8p.js -o ../out/ui8p_pack.js
+browserify -r ./ui8p.js -o ../out/ui8p_pack.js
 
 # Return
 cd ..
