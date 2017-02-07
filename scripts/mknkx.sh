8d1cd41758261a72c95416a8cd21828ac88f01af
0xFireball <0xFireball@outlook.com>
2017-02-06 20:21:39 -0800

    add polyfill import to build packed compat

---
+++ scripts/mknkx.sh
@@ -0,0 +1,18 @@
+
+# setup
+
+mkdir out || cd lib
+
+# browserify
+
+browserify ./jsdom_nkx.js -o ../out/jsdom_nkx_pack.js
+browserify ./ui8p.js -o ../out/ui8p_pack.js
+
+# Return
+cd ..
+
+# Transpile
+babel out/jsdom_nkx_pack.js -o out/jsdom_nkx.js
+
+# Compress
+uglifyjs out/jsdom_nkx.js -o out/jsdom_nkx.min.js -c
